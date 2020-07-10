1c8d53d0ed2d36bf4ce199646495a45641a71797
Brian Sterling <sterling000@gmail.com>
2020-07-10 11:14:14 -0700

    trying to fix sls invoke test

--- services/user-service/src/tests/createUser.js
+++ services/user-service/src/tests/createUser.js
@@ -5,7 +5,7 @@
 
 const mochaPlugin = require('serverless-mocha');
 const expect = mochaPlugin.chai.expect;
-let wrapped = mochaPlugin.getWrapper('createUser', '../../../services/user-service/src/functions/createUser.js', 'createUser');
+let wrapped = mochaPlugin.getWrapper('createUser', '/services/user-service/src/functions/createUser.js', 'createUser');
 const AWS = require('aws-sdk-mock');
 const AWS_SDK = require('aws-sdk');
 AWS.setSDKInstance(AWS_SDK);
