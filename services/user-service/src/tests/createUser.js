ec6b31e24fc5d1f1f33c0d6225cdef47b91ab8b1
Brian Sterling <sterling000@gmail.com>
2020-07-15 13:19:42 -0700

    fixing tests for mocha services/user-service/src/tests/

--- services/user-service/src/tests/createUser.js
+++ services/user-service/src/tests/createUser.js
@@ -9,13 +9,26 @@ let wrapped = mochaPlugin.getWrapper('createUser', '../../../services/user-servi
 const AWS = require('aws-sdk-mock');
 const AWS_SDK = require('aws-sdk');
 AWS.setSDKInstance(AWS_SDK);
+
 describe('createUser', () => {
   before((done) => {
+    AWS.mock('DynamoDB.DocumentClient', 'put', function (params, callback){
+      callback(null, "successfully put item in database");
+    });
+    done();
+  });
+  after((done) => {
+    AWS.restore('DynamoDB.DocumentClient');
     done();
   });
 
   it('implement tests here', async () => {
-    const response = await wrapped.run({});
+    process.env.DYNAMODB_USER_TABLE = 'user-service-userTable-dev';
+    const response = await wrapped.run({
+      body:JSON.stringify({
+      username: 'test',
+      password: '123'
+    })});
     expect(response).to.not.be.empty;
   });
 });
